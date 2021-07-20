8f14a8eb0d2ec49c1a79c44b0b4a6ad1deb8e8ce
KeithDinh <keithdinhcs@gmail.com>
2021-07-20 10:21:40 -0500

    add top revenue movie api

---
+++ MovieShopAPI/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using ApplicationCore.ServiceInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private IMovieService _movieService;
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+        // attribute based routing
+        [HttpGet]
+        [Route("toprevenue")]
+        public async Task<IActionResult> GetTopRevenueMovies()
+        {
+
+            var movies = await _movieService.GetTopRevenueMovies();
+            if (!movies.Any())
+            {
+                return NotFound("No movies found");
+            }
+            return Ok(movies);
+        }
+    }
+}
