2449346843364c69747858cb38b1753811c5aa13
KeithDinh <keithdinhcs@gmail.com>
2021-07-21 09:35:37 -0500

    add buy api

--- MovieShopAPI/Controllers/UserController.cs
+++ MovieShopAPI/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.Services;
+﻿using ApplicationCore.ServiceInterfaces;
+using Infrastructure.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,12 +14,24 @@ namespace MovieShopAPI.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly ICurrentUser _currentUser;
 
-        public UserController(IUserService userService)
+        public UserController(IUserService userService, ICurrentUser currenUser)
         {
             _userService = userService;
+            _currentUser = currenUser;
         }
-
+        [HttpPost]
+        [Route("purchase")]
+        public async Task<IActionResult> BuyMovie(int id)
+        {
+            if (!_currentUser.isAuthenticated)
+            {
+                return Unauthorized("Need to login first");
+            }
+            var movie = await _userService.BuyMovie(id);
+            return Ok(movie);
+        }
         [HttpPost]
         [Route("favorite")]
         public async Task<IActionResult> Favorite([FromBody] int movieId)
